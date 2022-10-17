a91eeb8498afa73c17e7ee4131d1f6857a3f5383
KobirBappy <kobirhossen115@gmail.com>
2022-10-17 17:47:01 +0600

    Firs Up

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:ecommerce_admin_panel/controllers/MenuController.dart';
+import 'package:ecommerce_admin_panel/controllers/auth_controller.dart';
+import 'package:ecommerce_admin_panel/controllers/orders_controller.dart';
+import 'package:ecommerce_admin_panel/controllers/product_controller.dart';
+import 'package:ecommerce_admin_panel/screens/main/main_screen.dart';
+import 'package:ecommerce_admin_panel/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'shared/remote/dio_helper.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDTkvvRk2T4SHbAzlMU1Bnx27L-Vm_ewR0", // Your apiKey
+      appId: "1:757724996815:web:12fb620b0d010033e183ba", // Your appId
+      messagingSenderId: "757724996815", // Your messagingSenderId
+      projectId: "compleate-ecom", // Your projectId
+    ),
+  );
+  await DioHelper.init();
+  // DesktopWindow.setMinWindowSize(Size(1300, 800));
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => AuthController(),
+    ),
+    ChangeNotifierProxyProvider<AuthController, MenuController>(
+      update: (context, auth, previousMenu) => MenuController(auth),
+      create: (BuildContext context) => MenuController(null),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => OrdersController()..getAllorders(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => ProductController()..getallProduct(),
+    ),
+  ], child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ecommerce Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
